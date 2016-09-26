aec7e96fb8dc0bdd8418a3759efe73f5dded88b8
Your Name <you@example.com>
2016-09-26 15:53:17 +0800

    update

--- background.js
+++ background.js
@@ -1,41 +1,73 @@
-var __state;
-chrome.extension.onConnect.addListener(function(port) {
-  port.onMessage.addListener(function(msg) {
-    if(msg==='reload'){
-      msg='__state';
-      __state=false;
-    }
-    if(msg==='state'){
-      var m='';
-      getState()(function(s){
-        if(s.length>400){
-          m=s;
-        }
-        port.postMessage(m);
-      })
-    }
-  });
+var parser = new DOMParser();
+chrome.extension.onConnect.addListener(function (port) {
+	port.onMessage.addListener(function (msg) {
+		var type = msg.data.type || msg.data;
+		var qid = msg.qid;
+		var param;
+		if (typeof  msg.data === 'object') {
+			param = msg.data;
+			delete  param.type;
+		}
+		var result=0;
+		switch (type) {
+			case 'list':
+				 fetchDoc('http://req.51qqt.com/Pts/issuelist.aspx?project=10', 0, function (dom) {
+					if (typeof dom==='object') {
+						var fs = [].slice.call(dom.querySelectorAll('.problem_title_cell>a[href^="ViewProblem.aspx?Problem"]'));
+						if (fs.length) {
+							result=[];
+							fs.forEach(function (a) {
+								result.push({
+									url: 'http://req.51qqt.com/Pts/LogWork'+a.getAttribute('href').replace('ViewProblem',''),
+									text: a.textContent
+								})
+							})
+						}
+					}
+					port.postMessage({
+						qid: qid,
+						data: result
+					});
+				});
+				break;
+			case 'send':
+				fetchDoc(param.url,0,function(dom){
+					var state = dom.forms[0].__VIEWSTATE.value;
+					fetchDoc(param.url,{
+						__VIEWSTATE:state,
+						ctl00$CP1$btnSubbmit:'提交工作记录 (Alt+S)',
+						ctl00$CP1$editorContent$editor:param.content
+					},function(r){
+						port.postMessage({
+							qid: qid,
+							data: typeof r==='object'||r<400
+						});
+					})
+				});
+		}
+	});
 });
 
-var getState = function(){
-  var cbs;
-  if(__state){
-    setTimeout(function(){
-      if(cbs)cbs(__state);
-    },0)
-  }else{
-    fetch('http://req.51qqt.com/Pts/LogWork.aspx?problem=957',{
-      credentials: 'include'
-    })
-        .then(function(res){console.log(res);return res.text()})
-        .then(function(text){
-          var parser = new DOMParser();
-          var v = parser.parseFromString(text,'text/html').forms[0].__VIEWSTATE.value;
-          __state=v;
-          if(cbs)cbs(v);
-        })
-  }
-  return function (c){
-    cbs=c
-  }
+var fetchDoc = function (url, params, func) {
+	var request = new XMLHttpRequest();
+	var data;
+	if (params) {
+		data = new FormData();
+		Object.keys(params).forEach(function (k) {
+			data.append(k, params[k]);
+		});
+	}
+	request.open(params ? "POST": "GET", url);
+	request.onreadystatechange = function () {
+		if (request.readyState === XMLHttpRequest.DONE) {
+			if (request.status === 200) {
+				var dom = parser.parseFromString(request.responseText, 'text/html');
+				func(dom);
+			} else {
+				func(request.status)
+			}
+		}
+	};
+	request.send(data);
 };
+
--- popup.js
+++ popup.js
@@ -1,105 +1,111 @@
-var loginWin = document.querySelector('.needLogin');
-var login = document.createElement('iframe');
-var url = document.querySelector('#url');
-var splice = document.querySelector('#splice');
-var f = document.querySelector('#f');
-var btn_send = document.querySelector('#send');
-var btn_clear = document.querySelector('#clear');
-var btn_check = document.querySelector('#check');
+var $ = document.querySelector.bind(document);
+var $new = document.createElement.bind(document);
+var loginWin = $('.needLogin');
+var login = $new('iframe');
+var url = $('#url');
+var splice = $('#splice');
+var btn_send = $('#send');
+var btn_clear = $('#clear');
+var btn_check = $('#check');
+var editor;
+var ck = chrome.cookies;
 var port = chrome.extension.connect({name: 'Job Logs'});
-var _state;
-var logsUrl = 'http://req.51qqt.com/Pts/LogWork.aspx?problem=';
-var pgId;
-chrome.cookies.onChanged.addListener(function(changeInfo) {
-	checkLogin();
+var cbList = {};
+
+var onMsg = function (msg) {
+	var qid = msg.qid;
+	var func = cbList[qid];
+	delete cbList[qid];
+	func(msg.data);
+};
+
+var eventsList = [];
+ck.onChanged.addListener(function () {
+	eventsList.forEach(function (func) {
+		func();
+	});
 });
-var checkLogin = function(func){
-	chrome.cookies.get({
-		name:'.URTracker',
-		url:'http://req.51qqt.com'
-	},function(cookie){
-		if(!cookie){
-			_state='';
+
+var checkLogin = function (func) {
+	ck.get({
+		name: '.URTracker',
+		url: 'http://req.51qqt.com'
+	}, function (cookie) {
+		// 登录cookie检查
+		if (!cookie) {
 			loginWin.show();
-		}else {
+			if (0 === eventsList.length) {
+				eventsList.push(function () {
+					eventsList.length = 0;
+					checkLogin(loadList)
+				})
+			}
+		} else {
+			eventsList.length = 0;
+			if (func)func();
 			loginWin.hide();
-			if(func)func();
 		}
 	})
 };
-checkLogin();
-try {
-	tinymce.init({
-		selector:'textarea' ,
-		plugins :'',
-		menubar: false,
-		selection_toolbar: false,
-		min_height:400,
-		max_height:600
-	}).then(function(editor){
-		editor=editor[0];
-		var ee = editor.getBody();
-		var start = ee.innerHTML;
-		btn_clear.onclick =function(){
-			ee.innerHTML=start;
-			localStorage.removeItem('editor');
-		};
-		var cc = localStorage.getItem('editor');
-		if(cc){
-			ee.innerHTML = cc;
-		}
-		var save = function(){
-			var t = ee.textContent.replace(/^\s+|\s+$/,'');
-			if(!t)return;
-			localStorage.setItem('editor',ee.innerHTML)
-		};
-		editor.on('keyup',save)
+var loadList = function () {
+	ck.remove({
+		url: 'http://req.51qqt.com',
+		name:'LastQueryString_27_10'
 	});
-}catch (e){}
-
-
-if(pgId=localStorage.getItem('pgId')){
-	url.value = pgId
-}
-url.onblur = function(){
-	localStorage.setItem('pgId',url.value);
+	ck.remove({
+		url: 'http://req.51qqt.com',
+		name:'CurrentProblemList'
+	});
+	ck.set(ck1);
+	ck.set(ck2);
+	url.onchange=null;
+	var _u = localStorage.getItem('url');
+	url.value = undefined;
+	query('list', function (array) {
+		if (!array)alert('获取日志列表失败');
+		else {
+			url.innerHTML = '';
+			var k = document.createDocumentFragment();
+			var defaultIndex = 0;
+			array.forEach(function (d, i) {
+				var o = $new('option');
+				o.value = d.url;
+				o.text = d.text;
+				k.appendChild(o);
+				if (d.url === _u)defaultIndex = i;
+			});
+			url.appendChild(k);
+			url.onchange=function(){
+				localStorage.setItem('url',url.value);
+			};
+			url.selectedIndex = defaultIndex;
+		}
+	})
 };
-
-loginWin.show = function(){
-	loginWin.style.display='block';
-	login.id='login';
-	login.src='http://req.51qqt.com/Accounts/login.aspx';
+loginWin.show = function () {
+	loginWin.style.display = 'block';
+	login.id = 'login';
+	login.src = 'http://req.51qqt.com/Accounts/login.aspx';
 	splice.appendChild(login);
-	setTimeout(function(){
-		loginWin.style.opacity=1;
-	},30)
+	setTimeout(function () {
+		loginWin.style.opacity = 1;
+	}, 30)
 };
-loginWin.hide = function(){
-	if(_state)reloadState();
-	loginWin.style.opacity=0;
-	setTimeout(function(){
-		loginWin.display='';
-		if(login.parentNode){
+loginWin.hide = function () {
+	loginWin.style.opacity = 0;
+	setTimeout(function () {
+		loginWin.style.display = '';
+		if (login.parentNode) {
 			splice.removeChild(login);
 		}
-	},210);
+	}, 210);
 };
-
-port.onMessage.addListener(function(state) {
-	if(state){
-		_state=state;
-	}
-});
-var bg = function(msg){
-	port.postMessage(msg);
+var query = function (param, func) {
+	var qid = Date.now() + Math.random();
+	cbList[qid] = func;
+	port.postMessage({qid: qid, data: param});
 };
-var getState =function(){
-	bg('state');
-};
-var reloadState = function(){
-	bg('reload');
-};
-var encode = function(a) {
+var encode = function (a) {
 	if (!a)
 		return '';
 	a = a.replace(/&/g, '&amp;');
@@ -107,33 +113,82 @@ var encode = function(a) {
 	a = a.replace(/>/g, '&gt;');
 	return a;
 };
-
-function send(){
-	var u;
+var _send;
+var send = function () {
+	if(_send)return;
+	_send=true;
 	var editor = tinymce.EditorManager.activeEditor.getBody();
-	var t = editor.textContent.replace(/^\s+|\s+$/,'');
-	if(!t)return;
-	checkLogin(function(){
+	var t = editor.textContent.replace(/^\s+|\s+$/, '');
+	if (!t)return;
+	checkLogin(function () {
 		var content = encode(editor.innerHTML);
-		if(!parseInt(u=url.value)){
-			return alert('请先设定日志ID');
-		}
-		f.ctl00$CP1$editorContent$editor.value = content;
-		var data = new FormData(f);
-
-		var request = new XMLHttpRequest();
-		request.open("POST", logsUrl+u);
-		request.onreadystatechange = function () {
-			if(request.readyState === XMLHttpRequest.DONE ) {
-				if(request.status <400)alert('提交成功！');
-				else{
-					alert('提交失败')
-				}
-			}
-		};
-		request.send(data);
+		query({
+			url: url.value,
+			type: 'send',
+			content: content
+		}, function (suc) {
+			alert(suc ? '提交成功' : '提交失败');
+			_send=false;
+		})
 	});
-}
-getState();
-btn_check.onclick = reloadState;
-btn_send.onclick=send;
+};
+btn_check.onclick = loadList;
+btn_send.onclick = send;
+port.onMessage.addListener(onMsg);
+
+// 我提交过的
+var ck1 = {
+	url: 'http://req.51qqt.com',
+	domain: 'req.51qqt.com',
+	httpOnly: false,
+	name: 'LastQueryString_27_10',
+	path: '/',
+	sameSite: 'no_restriction',
+	storeId: '0',
+	value: 'FilterType=1&procName=Relate_3&Title=%e6%88%91%e5%88%9b%e5%bb%ba%e7%9a%84%e4%ba%8b%e5%8a%a1'
+};
+// 时间排序
+var ck2 = {
+	url: 'http://req.51qqt.com',
+	domain: 'req.51qqt.com',
+	expirationDate: 253402300529.52216,
+	httpOnly: false,
+	name: 'ProblemListSortExpression_27_10',
+	path: '/',
+	sameSite: 'no_restriction',
+	storeId: '0',
+	value: 'LastTime DESC'
+};
+
+window.addEventListener("unload", function () {
+	localStorage.setItem('url', url.value);
+	if (editor) {
+		var ee = editor.getBody();
+		var t = ee.textContent.replace(/^\s+|\s+$/, '');
+		if (!t)return;
+		localStorage.setItem('editor', ee.innerHTML)
+	}
+}, true);
+
+tinymce.init({
+	selector: 'textarea',
+	menubar: false,
+	selection_toolbar: false,
+	min_height: 400,
+	max_height: 600,
+	plugins: 'paste',
+	paste_data_images: true
+}).then(function (editors) {
+	editor = editors[0];
+	var ee = editor.getBody();
+	var start = ee.innerHTML;
+	btn_clear.onclick = function () {
+		ee.innerHTML = start;
+		localStorage.removeItem('editor');
+	};
+	var cc = localStorage.getItem('editor');
+	if (cc) {
+		ee.innerHTML = cc;
+	}
+	checkLogin(loadList);
+});
